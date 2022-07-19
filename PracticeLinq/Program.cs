f1f3c4df70b9fe702627c6bfdabd854e43abc2e0
gamejunkiez <queenangrylobster@gmail.com>
2022-07-19 09:39:10 -0500

    Created and sorted list of games

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var gameNames = new List<string>() { "Super Mario Bros", "Dead By Daylight", "Pac-Man", "Roblox", "Minecraft", "Call of Duty", "Revenge of the Sunfish, Ultimate Extraordinary Edition" };
+            var sortedNames = gameNames.OrderBy(x => x.Length);
+            foreach(var name in sortedNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
